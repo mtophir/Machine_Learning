ccd384ca8dde7fbbe1aac74b140bd9a2e9a0a903
mtophir <45858737+mtophir@users.noreply.github.com>
2020-02-07 15:02:44 -0600

    Update and rename 3-layer-nn.py to numpy_3-layer-nn.py
    numpy_3-layer-nn.py

--- numpy_3-layer-nn.py
+++ numpy_3-layer-nn.py
@@ -1,12 +1,13 @@
-#3-layer-nn supervised learning
+#3-layer-nn supervised learning using numpy
 import numpy as np
 
 def nonlin(x,deriv=False):
         if(deriv==True):
             return x*(1-x)
         return 1/(1+np.exp(-x))
-    
+# input    
 X = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
+# expected output (supervised learning)
 y = np.array([[0],[1],[1],[0]])
 
 np.random.seed(1)
